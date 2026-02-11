99bc8c245f4f31224e0b807a72b8ecd426d06885
Vadim <_s_e_t__@ukr.net>
2026-02-11 15:57:01 +0200

    add a new page About

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 const Home = () => {
+  console.log('server render')
   return (
     <main>
-      <Image src='/icon.png' alt="icon" width={10} height={10} />
+      <Image src='/icon.png' alt="icon" width={100} height={100} />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
     </main>
